0daa0d6a3919043806080df1dd5438943f85df15
Kaden Barlow <kadenbarlow@gmail.com>
2024-10-08 18:28:12 -0600

    remove a bunch of files

--- nvim/lua/core/theme/init.lua
+++
@@ -1,5 +0,0 @@
--- vim.cmd.source('~/.dotfiles/nvim/lua/core/theme/dark-matter.vim')
--- vim.cmd.source('~/.dotfiles/nvim/lua/core/theme/github.vim')
-vim.cmd [[colorscheme tokyonight-storm]]
-vim.cmd.source('~/.dotfiles/nvim/lua/core/theme/tweaks.vim')
-vim.cmd.source('~/.dotfiles/nvim/lua/core/theme/transparency.vim')
--- nvim/lua/plugins/airline.lua
+++
@@ -1,5 +0,0 @@
-vim.g.airline_powerline_fonts = true
-vim.g.airline_symbols_ascii = true
--- vim.g.airline_left_sep = ' '
--- vim.g.airline_right_sep = ' '
-vim.g.airline_theme = 'deus'
